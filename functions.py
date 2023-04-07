4829faedf4c1433a824e44dc2b9e0a75a63bd59f
Obl1Que <s.dezhin@mail.ru>
2023-04-07 23:09:13 +0300

    Добавлен класс `logger` в `functions.py`, который сохраняет логи в отдельный файл `requests.log`. Также добавлены функции `open_log`, `save_log`, `readJSON`, `saveJSON` для удобста работы с файлами.

--- functions.py
+++ functions.py
@@ -1,5 +1,43 @@
 import random
 import string
+import os
+import json
+
+
+class logger:
+    def __init__(self):
+        self.log_c = readJSON("settings_glob.json")["log_c"]
+        self.massive = []
+
+    def add(self, _string, _type = "n"):
+        self.massive.append(f"[{self.log_c}][{_type}] {_string}")
+    def save(self, _path):
+        save_log(_path, self.massive)
+        self.massive.clear()
+        self.log_c += 1
+        data = readJSON("settings_glob.json")
+        data["log_c"] += 1
+        saveJSON("settings_glob.json", data)
+def open_log(path):
+    try:
+        with open(os.path.abspath(path), encoding="utf-8") as f:
+            return f.read()
+    except Exception as e:
+        print(f"PATH не найден!\n{e}")
+def save_log(path, new_log):
+    try:
+        with open(os.path.abspath(path), mode="a+", encoding="utf-8") as f:
+            for _ in new_log:
+                f.write(f"{_}\n")
+    except Exception as e:
+        print(e)
+
+def readJSON(path):
+    with open(os.path.abspath(path), encoding="utf-8") as f:
+        return json.load(f)
+def saveJSON(path, sdata):
+    with open(os.path.abspath(path), mode="w", encoding="utf-8") as f:
+        json.dump(sdata, f, indent=4)
 
 def generate_string():
     this_string = ''
