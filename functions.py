ae882a9344b2c3941d42a3ff55d90b10fa287cd3
Obl1Que <s.dezhin@mail.ru>
2023-04-10 00:08:28 +0300

    Добавлено определение времени запроса и его запись в лог-файл

--- functions.py
+++ functions.py
@@ -9,8 +9,8 @@ class logger:
         self.log_c = readJSON("settings_glob.json")["log_c"]
         self.massive = []
 
-    def add(self, _string, _type = "n"):
-        self.massive.append(f"[{self.log_c}][{_type}] {_string}")
+    def add(self, _string, time, _type = "n"):
+        self.massive.append(f"[{self.log_c}][{_type}][{time}] {_string}")
     def save(self, _path):
         save_log(_path, self.massive)
         self.massive.clear()
@@ -46,9 +46,3 @@ def generate_string():
         this_string += random.choice(list(string.ascii_lowercase + '0123456789'))
 
     return (this_string)
-
-# log = logger()
-# log.add("SELECT * FROM users")
-# log.add("INSERT INTO users VALUES ('123', 2)")
-# log.add("SELECT some_string FROM users")
-# log.save("requests.log")
